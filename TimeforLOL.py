2600bce2bd68baa7e2c5ba5af57e7b75965d390d
Samurai <1157290948@qq.com>
2021-01-26 00:01:02 +0800

    以管理员权限修改系统时间

--- TimeforLOL.py
+++ TimeforLOL.py
@@ -1,28 +1,16 @@
 from pynput.keyboard import Listener, Key, Controller
 # import logging
 import win32clipboard as w
-# import win32con
 import time
-
-
 # import requests
 
 
-# wenjianweizhi = "D:\\hi\\"
-
-# logging.basicConfig(filename=(wenjianweizhi + "keylogger.txt"), format="%(asctime)s:%(message)s", level=logging.DEBUG)
-
 # def download_page(url):
-#     headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:61.0) Gecko/20100101 Firefox/61.0"}
-#     r = requests.get(url, headers=headers)  # 增加headers, 模拟浏览器
-#     return r.text.replace('<br />', '') + "\r \n"
+#    headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:61.0) Gecko/20100101 Firefox/61.0"}
+#    r = requests.get(url, headers=headers)  # 增加headers, 模拟浏览器
+#    return r.text.replace('<br />','')+"\r \n"
 
 
-# def getClipboard():#读取剪切板
-#     w.OpenClipboard()
-#     d = w.GetClipboardData(win32con.CF_TEXT)
-#     w.CloseClipboard()
-#     return d
 def setClipboard(aString):  # 写入剪切板
     w.OpenClipboard()
     w.EmptyClipboard()
@@ -36,7 +24,7 @@ def printtoboard(string):
     keyboard.press(Key.ctrl)
     keyboard.press('v')
     # 延时10毫秒
-    time.sleep(0.001)
+    time.sleep(0.005)
     keyboard.release(Key.ctrl)
     keyboard.release('v')
 
@@ -44,11 +32,9 @@ def printtoboard(string):
 def timeinitial():
     time_1 = time.time()
     seconds = time_1 - time_0 + 300
-
     m, s = divmod(seconds, 60)
     m = int(m)
     s = int(s)
-    # 在这里调接口，将数据写进剪贴板，然后模拟键盘的粘贴（Ctrl + V）
     if m < 10:
         minute = '0' + str(m)
     else:
@@ -66,40 +52,48 @@ def press(key):
         print(key.char)
         if key.char == ',':
             print('进来了  --  ')
-            # url = 'https://nmsl.shadiao.app/api.php?level=min&lang=zh_cn'
-            # html = download_page(url)
             string = '上单闪现' + '->' + timeinitial()
             printtoboard(string)
         if key.char == '.':
             print('进来了  --  ')
-            # url = 'https://nmsl.shadiao.app/api.php?level=min&lang=zh_cn'
-            # html = download_page(url)
             string = '中路闪现' + '->' + timeinitial()
             printtoboard(string)
         if key.char == '/':
             print('进来了  --  ')
-            # url = 'https://nmsl.shadiao.app/api.php?level=min&lang=zh_cn'
-            # html = download_page(url)
             string = '打野闪现' + '->' + timeinitial()
             printtoboard(string)
         if key.char == ';':
             print('进来了  --  ')
-            # url = 'https://nmsl.shadiao.app/api.php?level=min&lang=zh_cn'
-            # html = download_page(url)
             string = 'ad闪现' + '->' + timeinitial()
             printtoboard(string)
         if key.char == "'":
             print('进来了  --  ')
-            # url = 'https://nmsl.shadiao.app/api.php?level=min&lang=zh_cn'
-            # html = download_page(url)
             string = '辅助闪现' + '->' + timeinitial()
             printtoboard(string)
-
+        # if key.char == '-':
+        #     print('进来了  --  ')
+        #     url = 'https://nmsl.shadiao.app/api.php?level=min&lang=zh_cn'
+        #     html = download_page(url)
+        #     string = html
+        #     printtoboard(string)
     except Exception as e:
-        print("已调到该程序，但是引用报错", e)
+        pass
 
 
-time_0 = time.time()
+def on_release(key):
+    try:
+        if key.char == '=':
+            print(f'{key} 被释放了')
+            return False
+    except Exception as e:
+        pass
 
-with Listener(on_press=press) as listener:
-    listener.join()
+
+number = 0
+
+
+while number < 10:
+    time_0 = time.time()
+    with Listener(on_press=press, on_release=on_release) as listener:
+        listener.join()
+    number += 1
